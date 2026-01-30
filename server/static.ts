29027e5ac7d31735401e17170033caec0cca3343
309empire <testanton446@gmail.com>
2026-01-30 14:48:42 +0100

    Update static.ts

--- server/static.ts
+++ server/static.ts
@@ -1,24 +1,22 @@
 import express, { type Express } from "express";
 import fs from "fs";
-const distPath = path.resolve(__dirname, "../dist/public");
+import path from "path"; // <- make sure this is here
 import { fileURLToPath } from "url";
 
 export function serveStatic(app: Express) {
-  // ESM-kompatibler __dirname Ersatz
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
 
-  const distPath = path.resolve(__dirname, "public");
+  const distPath = path.resolve(__dirname, "../dist/public");
   if (!fs.existsSync(distPath)) {
     throw new Error(
-      `Could not find the build directory: ${distPath}, make sure to build the client first`
+      `Could not find the build directory: ${distPath}, make sure to build the client first`,
     );
   }
 
   app.use(express.static(distPath));
 
-  // Fallthrough zu index.html, falls die Datei nicht existiert
-  app.use("*", (_req, res) => {
+  app.use("/*", (_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
